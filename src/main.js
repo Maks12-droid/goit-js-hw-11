71e50c50d80e59629d4acf8c43b102d98393959d
Maks12-droid <127047393+Maks12-droid@users.noreply.github.com>
2024-03-22 21:29:36 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 import { fetchImg } from './js/pixabay-api';
 
 let imgset;
-let searchImgs; 
 
 const inputfield = document.querySelector('input');
 const fillForm = document.querySelector('form');
@@ -16,60 +15,62 @@ const setGallery = document.querySelector('ul.gallery');
 const preloader = document.querySelector('.preloader');
 
 const showLoader = () => {
-  preloader.style.display = 'flex';
+  preloader.style.display = 'flex';
 };
 
 const hideLoader = () => {
-  preloader.style.display = 'none';
+  preloader.style.display = 'none';
 };
 
 const handleLoad = () => {
-  document.body.classList.add('loaded');
-  document.body.classList.remove('loaded_hiding');
+  document.body.classList.add('loaded');
+  document.body.classList.remove('loaded_hiding');
 };
 
 window.onload = handleLoad;
 
 fillForm.addEventListener('submit', async event => {
-  event.preventDefault();
-  
-  searchImgs = event.currentTarget.elements.search.value.trim();
+  event.preventDefault();
+  
+  const searchImgs = event.currentTarget.elements.search.value.trim();
 
-  if (!searchImgs.length) {
-    iziToast.error({
-      color: 'yellow',
-      message: ` Please fill in the field for search query.`,
-      position: 'topRight',
-    });
-    setGallery.innerHTML = '';
-    return;
-  }
+  if (!searchImgs.length) {
+    iziToast.error({
+      color: 'yellow',
+      message: ` Please fill in the field for search query.`,
+      position: 'topRight',
+    });
+    setGallery.innerHTML = '';
+    return;
+  }
 
-  showLoader();
+  showLoader();
 
-  try {
-    const images = await fetchImg(searchImgs);
-    imgset = images.hits;
+  try {
+    const images = await fetchImg(searchImgs);
+    imgset = images.hits;
 
-    if (!imgset.length) {
-      iziToast.error({
-        color: 'red',
-        message: `❌ Sorry, there are no images matching your search query. Please try again!`,
-        position: 'topRight',
-      });
-    } else {
+    if (!imgset.length) {
+      iziToast.error({
+        color: 'red',
+        message: `❌ Sorry, there are no images matching your search query. Please try again!`,
+        position: 'topRight',
+      });
+    } else {
 
-    }
-  } catch (error) {
-    iziToast.error({
-      color: 'red',
-      message: `:x: Sorry, there was a mistake. Please try again!`,
-      position: 'topRight',
-    });
-  } finally {
-    hideLoader();
-    handleLoad();
-  }
+    }
+  } catch (error) {
+    iziToast.error({
+      color: 'red',
+      message: `:x: Sorry, there was a mistake. Please try again!`,
+      position: 'topRight',
+    });
+  } finally {
+    hideLoader();
+    handleLoad();
+  }
 });
+
+export { searchImgs };
 
 
