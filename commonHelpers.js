fc60fbf14357d64954d4eeed4ff426c740e4f7c4
Maks12-droid <127047393+Maks12-droid@users.noreply.github.com>
2024-03-22 17:59:19 +0000

    Deploying to gh-pages from @ Maks12-droid/goit-js-hw-11@553c82bd0397bbe0abd4974498a8b7a43dbc1b43 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,26 +1,26 @@
-import{S as u,i as l}from"./assets/vendor-5b791d57.js";(function(){const s=document.createElement("link").relList;if(s&&s.supports&&s.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))t(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const i of r.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&t(i)}).observe(document,{childList:!0,subtree:!0});function o(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function t(e){if(e.ep)return;e.ep=!0;const r=o(e);fetch(e.href,r)}})();function m(a){c.innerHTML="";const s=d.map(t=>`<li class="img-blok">
-        <a href="${t.largeImageURL}">     
-    <img  src="${t.webformatURL}"
-    data-source="${t.largeImageURL}"
-    alt="${t.tags}">
+import{S as h,i as a}from"./assets/vendor-5b791d57.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))r(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const n of o.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&r(n)}).observe(document,{childList:!0,subtree:!0});function s(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function r(e){if(e.ep)return;e.ep=!0;const o=s(e);fetch(e.href,o)}})();function m(i){l.innerHTML="";const t=c.map(r=>`<li class="img-blok">
+        <a href="${r.largeImageURL}">     
+    <img  src="${r.webformatURL}"
+    data-source="${r.largeImageURL}"
+    alt="${r.tags}">
    
     <ul class="image-descript">
   <li>
     <h3>likes</h3>
-    <p>${t.likes}</p>
+    <p>${r.likes}</p>
   </li>
   <li>
     <h3>views</h3>
-    <p>${t.views}</p>
+    <p>${r.views}</p>
   </li>
   <li>
     <h3>comments</h3>
-    <p>${t.comments}</p>
+    <p>${r.comments}</p>
   </li>
   <li>
     <h3>downloads</h3>
-    <p>${t.downloads}</p>
+    <p>${r.downloads}</p>
   </li>
     </ul>
-  </a></li>`).join("");c.insertAdjacentHTML("beforeend",s),new u(".gallery a",{captionsData:"alt"}).refresh()}function f(){const s=`https://pixabay.com/api/?${new URLSearchParams({key:"22926721-5d20aa08498ffd1ff2f906542",q:n,image_type:"photo",orientation:"horizontal",safesearch:"true"})}`;return fetch(s).then(o=>{if(!o.ok)throw new Error(o.status);return o.json()})}const c=document.querySelector("ul.gallery");let d,n;document.querySelector("input");const h=document.querySelector("form");document.querySelector(".preloader");const p=()=>{document.body.classList.add("loaded"),document.body.classList.remove("loaded_hiding")};window.onload=p;h.addEventListener("submit",async a=>{a.preventDefault(),n=new FormData(a.currentTarget).get("search");try{const o=await f(n);d.length||l.error({color:"red",message:"❌ Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),m(o)}catch{l.error({color:"red",message:":x: Sorry, there was a mistake. Please try again!",position:"topRight"})}finally{}});
+  </a></li>`).join("");l.insertAdjacentHTML("beforeend",t),new h(".gallery a",{captionsData:"alt"}).refresh()}function p(){const t=`https://pixabay.com/api/?${new URLSearchParams({key:"22926721-5d20aa08498ffd1ff2f906542",q:d,image_type:"photo",orientation:"horizontal",safesearch:"true"})}`;return fetch(t).then(s=>{if(!s.ok)throw new Error(s.status);return s.json()})}const l=document.querySelector("ul.gallery");let c,d;const y=document.querySelector("input"),g=document.querySelector("button");document.querySelector("form");const u=document.querySelector(".preloader"),L=()=>{u.style.display="flex"},w=()=>{u.style.display="none"},f=()=>{document.body.classList.add("loaded"),document.body.classList.remove("loaded_hiding")};window.onload=f;g.addEventListener("click",async i=>{i.preventDefault(),d=y.value.trim(),d.length||(a.error({color:"yellow",message:" Please fill in the field for search query.",position:"topRight"}),l.innerHTML=""),L();try{const t=await p();c=t.hits,c.length||a.error({color:"red",message:"❌ Sorry, there are no images matching your search query. Please try again!",position:"topRight"}),m(t)}catch{a.error({color:"red",message:":x: Sorry, there was a mistake. Please try again!",position:"topRight"})}finally{w(),f()}});
 //# sourceMappingURL=commonHelpers.js.map
