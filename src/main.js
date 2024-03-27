0e0c6dea008000f7b2ade4ebc2cfec0528205f30
Maks12-droid <127047393+Maks12-droid@users.noreply.github.com>
2024-03-27 20:31:48 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -6,12 +6,8 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { fetchImg } from './js/pixabay-api';
 
-let searchImgs = '';
-
-const inputfield = document.querySelector('input');
 const fillForm = document.querySelector('form');
 const setGallery = document.querySelector('ul.gallery');
-
 const preloader = document.querySelector('.preloader');
 
 const showLoader = () => {
@@ -32,13 +28,22 @@ window.onload = handleLoad;
 fillForm.addEventListener('submit', async event => {
   event.preventDefault();
 
-  searchImgs = event.currentTarget.elements.search.value.trim();
+  const searchImgs = event.currentTarget.elements.search.value.trim();
+
+  if (!searchImgs) {
+    iziToast.error({
+      color: 'yellow',
+      message: ` Please fill in the field for search query.`,
+      position: 'topRight',
+    });
+    return;
+  }
 
   showLoader();
 
   try {
     const images = await fetchImg(searchImgs);
-    imgset = images.hits;
+    const imgset = images.hits;
 
     if (!imgset.length) {
       iziToast.error({
@@ -47,7 +52,7 @@ fillForm.addEventListener('submit', async event => {
         position: 'topRight',
       });
     } else {
-
+      renderImgs(imgset); // Викликаємо функцію для відображення зображень
     }
   } catch (error) {
     iziToast.error({
@@ -61,6 +66,45 @@ fillForm.addEventListener('submit', async event => {
   }
 });
 
-export { searchImgs };
+function renderImgs(images) {
+  setGallery.innerHTML = '';
 
+  const imgGallery = images
+    .map(
+      image => `<li class="img-blok">
+        <a href="${image.largeImageURL}">     
+          <img src="${image.webformatURL}"
+          data-source="${image.largeImageURL}"
+          alt="${image.tags}">
+          <ul class="image-descript">
+            <li>
+              <h3>likes</h3>
+              <p>${image.likes}</p>
+            </li>
+            <li>
+              <h3>views</h3>
+              <p>${image.views}</p>
+            </li>
+            <li>
+              <h3>comments</h3>
+              <p>${image.comments}</p>
+            </li>
+            <li>
+              <h3>downloads</h3>
+              <p>${image.downloads}</p>
+            </li>
+          </ul>
+        </a>
+      </li>`
+    )
+    .join('');
+
+  setGallery.insertAdjacentHTML('beforeend', imgGallery);
+
+  const lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+  });
+
+  lightbox.refresh();
+}
 
