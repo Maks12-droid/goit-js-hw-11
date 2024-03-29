38fe460d3dd91ea2309c190412cf2965dd74e9b2
Maks12-droid <127047393+Maks12-droid@users.noreply.github.com>
2024-03-29 21:54:45 +0200

    1

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,22 +1,20 @@
 import {searchImgs } from '../main';
 
-export function fetchImg() {
+export function fetchImg(query) {
+  const BASE_URL = "https://pixabay.com/api/"
   const searchParams = new URLSearchParams({
     key: '22926721-5d20aa08498ffd1ff2f906542',
     // key: '42609290-856768105ab9e79485c69bf61',
-    q: searchImgs,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: 'true',
+    q: query,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
   });
-
-  const url = `https://pixabay.com/api/?${searchParams}`;
-
+  const url = `${BASE_URL}?${searchParams}`;
   return fetch(url).then(response => {
     if (!response.ok) {
       throw new Error(response.status);
     }
-
     return response.json();
   });
 }
