2dc72873e8075f759535fa449c03af10e9582fd1
Maks12-droid <127047393+Maks12-droid@users.noreply.github.com>
2024-03-29 21:58:44 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,52 +3,60 @@ import 'izitoast/dist/css/iziToast.min.css';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 import { renderImgs } from './js/render-functions';
-import {fetchImg}  from './js/pixabay-api.js'
+import { fetchImg } from './js/pixabay-api.js';
+
 let searchImgs = '';
+
 const inputfield = document.querySelector('input');
 const fillForm = document.querySelector('form');
 const preloader = document.querySelector('.preloader');
+
 const showLoader = () => {
   preloader.style.display = 'flex';
 };
+
 const hideLoader = () => {
   preloader.style.display = 'none';
 };
+
 const handleLoad = () => {
   document.body.classList.add('loaded');
   document.body.classList.remove('loaded_hiding');
 };
+
 window.onload = handleLoad;
-fillForm.addEventListener('submit',  event => {
+
+fillForm.addEventListener('submit', event => {
   event.preventDefault();
   const input = inputfield.value.trim();
   if (input !== '') {
-       // тут додайте лоадер
-      fetchImg(input)
-          .then((photos) => {
-            renderImgs(photos.hits);
-              // тут додайте лоадер
-          })
-          .catch((error) => {
-              console.log(error);
-              hideLoader();
-              iziToast.error({
-                  message: 'Sorry, an error occurred while loading. Please try again!',
-                  theme: 'dark',
-                  progressBarColor: '#FFFFFF',
-                  color: '#EF4040',
-                  position: 'topRight',
-              });
-          });
-          fillForm.reset();
-  } else {
-      iziToast.show({
-          message: 'Please complete the field!',
+    showLoader(); 
+    fetchImg(input)
+      .then((photos) => {
+        renderImgs(photos.hits);
+        hideLoader(); 
+      })
+      .catch((error) => {
+        console.error(error);
+        hideLoader(); 
+        iziToast.error({
+          message: 'Sorry, an error occurred while loading. Please try again!',
           theme: 'dark',
           progressBarColor: '#FFFFFF',
           color: '#EF4040',
           position: 'topRight',
+        });
       });
+    fillForm.reset();
+  } else {
+    iziToast.show({
+      message: 'Please complete the field!',
+      theme: 'dark',
+      progressBarColor: '#FFFFFF',
+      color: '#EF4040',
+      position: 'topRight',
+    });
   }
 });
+
 export { searchImgs };
