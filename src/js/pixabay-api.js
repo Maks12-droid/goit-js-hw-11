96e86f6918ecd2d34bfd843bf69e1bd8907e33d3
Maks12-droid <127047393+Maks12-droid@users.noreply.github.com>
2024-04-03 11:24:49 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,10 +1,7 @@
-import {searchImgs } from '../main';
-
 export function fetchImg(query) {
-  const BASE_URL = "https://pixabay.com/api/"
+  const BASE_URL = "https://pixabay.com/api/";
   const searchParams = new URLSearchParams({
     key: '22926721-5d20aa08498ffd1ff2f906542',
-    // key: '42609290-856768105ab9e79485c69bf61',
     q: query,
     image_type: "photo",
     orientation: "horizontal",
